f61661a94e6eccdbc74d06b1e70c1acd46a7903d
babysharkdududududududu <thanh89508@gmail.com>
2023-11-10 21:00:48 +0700

    pull family

---
+++ src/main/java/com/camera/projectcamera/repository/PropertiesRepository.java
@@ -0,0 +1,8 @@
+package com.camera.projectcamera.repository;
+
+import com.camera.projectcamera.entity.Properties;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PropertiesRepository extends JpaRepository<Properties, Long> {
+
+}
